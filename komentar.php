2825961331a910b48a0956e8524c73b23916df36
Deshinta Rizqa Rahmawati <43508097+DeshintaRizqaR@users.noreply.github.com>
2018-10-05 10:21:51 +0700

    komentar
    memberikan komentar,jika inputan tidak sesuai

--- komentar.php
+++ komentar.php
@@ -1,3 +1,36 @@
-<?php
+<form action="" method="POST">
+	<table>
+		<tr>
+			<td>Nim</td>
+			<td>:</td>
+			<td><textarea name="komentar"></textarea></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" value=""></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td><input type="text" name="email" value=""></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="Daftar" value="Daftar"></td>
+		</tr>
+		</table>
+	</form>
 
-?>
+	<?php 
+if (isset($_POST['kirim'])) {
+	$komentar = $_POST['komentar'];
+
+	if (str_word_count($komentar)<5) {
+		echo "Nim salah harus 10 karakter";
+	}else{
+		echo str_word_count($komentar);
+		
+		
+	}
+	}
+	 ?>

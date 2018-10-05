8f552dd18744379ffd179a43b172dbb1d38d7263
Deshinta Rizqa Rahmawati <43508097+DeshintaRizqaR@users.noreply.github.com>
2018-10-05 10:03:47 +0700

    registrasi.php
    form registrasi

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form action="" method="POST">
+	<table>
+		<tr>
+			<td>Nim</td>
+			<td>:</td>
+			<td><input type="text" name="nim" value=""></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" value=""></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td><input type="text" name="email" value=""></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="Daftar" value="Daftar"></td>
+		</tr>
+		</table>
+	</form>
 
+<?php 
 
+if (isset($_POST['Daftar'])) {
+	include 'prosesregistrasi.php';
+	
+}
 
-</body>
-</html>
+ ?>
